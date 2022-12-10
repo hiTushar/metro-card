a909e1a1859bce13892f2f6850b25f681ddb2989
Tushar Jain <tushar.jain@getvymo.com>
2022-12-10 23:57:35 +0530

    Add error handling

--- geektrust.js
+++ geektrust.js
@@ -3,35 +3,36 @@ const fs = require("fs")
 const filename = process.argv[2]
 
 fs.readFile(filename, "utf8", (err, data) => {
-    let inputLines = data.toString().split("\n").map(t => t.trim());
-    let passengers = {};
-    inputLines.forEach(command => {
-        if (command.includes("BALANCE")) {
-            let parsedCommmand = command.split(" ");
-            if ( passengers[parsedCommmand[1]] === undefined ) {
-                passengers[parsedCommmand[1]] = {};
+    if(err) {
+        throw err;
+    }
+    else {
+        let inputLines = data.toString().split("\n").map(t => t.trim());
+        let passengers = {};
+        inputLines.forEach(command => {
+            if (command.includes("BALANCE")) {
+                let parsedCommmand = command.split(" ");
+                if ( passengers[parsedCommmand[1]] === undefined ) {
+                    passengers[parsedCommmand[1]] = {};
+                }
+                passengers[parsedCommmand[1]].balance = Number(parsedCommmand[2]);
             }
-            passengers[parsedCommmand[1]].balance = Number(parsedCommmand[2]);
-        }
-
-        if (command.includes("CHECK_IN")) {
-            let parsedCommmand = command.split(" ");
-
-            if (passengers[parsedCommmand[1]] === undefined) {
-                passengers[parsedCommmand[1]] = {};
+    
+            if (command.includes("CHECK_IN")) {
+                let parsedCommmand = command.split(" ");
+    
+                if (passengers[parsedCommmand[1]] === undefined) {
+                    passengers[parsedCommmand[1]] = {};
+                }
+    
+                passengers[parsedCommmand[1]].type = parsedCommmand[2];
+    
+                if (passengers[parsedCommmand[1]].station === undefined) {
+                    passengers[parsedCommmand[1]].station = [];
+                }
+                passengers[parsedCommmand[1]].station.push(parsedCommmand[3]);
             }
-
-            passengers[parsedCommmand[1]].type = parsedCommmand[2];
-
-            if (passengers[parsedCommmand[1]].station === undefined) {
-                passengers[parsedCommmand[1]].station = [];
-            }
-            passengers[parsedCommmand[1]].station.push(parsedCommmand[3]);
-        }
-    })
-    console.log(passengers);
-    /*if (err) throw err
-    var inputLines = data.toString().split("\n")
-    // Add your code here to process input commands
-    */
+        })
+        console.log(passengers);
+    }
 })
