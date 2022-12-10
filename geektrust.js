6b9ff68b5f5f54cd5d8a4e4df7fc33400d2010fb
Tushar Jain <tushar.jain@getvymo.com>
2022-12-10 23:53:19 +0530

    Create passenger details object

--- geektrust.js
+++ geektrust.js
@@ -3,7 +3,33 @@ const fs = require("fs")
 const filename = process.argv[2]
 
 fs.readFile(filename, "utf8", (err, data) => {
-    console.log(filename);
+    let inputLines = data.toString().split("\n").map(t => t.trim());
+    let passengers = {};
+    inputLines.forEach(command => {
+        if (command.includes("BALANCE")) {
+            let parsedCommmand = command.split(" ");
+            if ( passengers[parsedCommmand[1]] === undefined ) {
+                passengers[parsedCommmand[1]] = {};
+            }
+            passengers[parsedCommmand[1]].balance = Number(parsedCommmand[2]);
+        }
+
+        if (command.includes("CHECK_IN")) {
+            let parsedCommmand = command.split(" ");
+
+            if (passengers[parsedCommmand[1]] === undefined) {
+                passengers[parsedCommmand[1]] = {};
+            }
+
+            passengers[parsedCommmand[1]].type = parsedCommmand[2];
+
+            if (passengers[parsedCommmand[1]].station === undefined) {
+                passengers[parsedCommmand[1]].station = [];
+            }
+            passengers[parsedCommmand[1]].station.push(parsedCommmand[3]);
+        }
+    })
+    console.log(passengers);
     /*if (err) throw err
     var inputLines = data.toString().split("\n")
     // Add your code here to process input commands
