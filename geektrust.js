7cd759b3ccde96e661ae81146494de0561e4c854
Tushar Jain <tushar.jain@getvymo.com>
2022-12-11 12:45:54 +0530

    Add console output - unsorted

--- geektrust.js
+++ geektrust.js
@@ -14,7 +14,30 @@ const SERVICE_CHARGE_RATE = 2;
 const DISCOUNT_RATE = 50;
 
 const STATION_DATA = {};
+/**
+ * sample STATION_DATA = {
+        CENTRAL: {
+            collection: 300,
+            discount: 0,
+            passengerTypes: { ADULT: 1, SENIOR_CITIZEN: 1, KID: 0 }
+        },
+        AIRPORT: {
+            collection: 403,
+            discount: 100,
+            passengerTypes: { ADULT: 2, SENIOR_CITIZEN: 0, KID: 2 }
+        }
+    }
+ */
 const PASSENGER_DATA = {};
+/**
+ * sample PASSENGER_DATA = {
+        MC1: { balance: 600, type: 'ADULT', station: [ 'CENTRAL', 'AIRPORT' ] },
+        MC2: { balance: 500, type: 'SENIOR_CITIZEN', station: [ 'CENTRAL' ] },
+        MC3: { balance: 50, type: 'KID', station: [ 'AIRPORT' ] },
+        MC4: { balance: 50, type: 'ADULT', station: [ 'AIRPORT' ] },
+        MC5: { balance: 200, type: 'KID', station: [ 'AIRPORT' ] }
+    }
+ */
 
 fs.readFile(filename, "utf8", (err, data) => {
     if(err) {
@@ -47,7 +70,6 @@ fs.readFile(filename, "utf8", (err, data) => {
             }
 
             if (command.includes("PRINT_SUMMARY")) {
-                console.log(PASSENGER_DATA);
                 Object.keys(PASSENGER_DATA).forEach(passenger => {
                     if(PASSENGER_DATA[passenger].station === undefined) return; // if the mentioned passenger did not mmake any journey
 
@@ -84,8 +106,17 @@ fs.readFile(filename, "utf8", (err, data) => {
                         STATION_DATA[station].passengerTypes[PASSENGER_DATA[passenger].type]++;
                     })
                 });
-                console.log(STATION_DATA);
-                
+
+                STATIONS.forEach(station => {
+                    currentStationData = STATION_DATA[station];
+                    console.log("TOTAL_COLLECTION", station, currentStationData.collection, currentStationData.discount);
+                    console.log("PASSENGER_TYPE_SUMMARY");
+                    Object.keys(currentStationData.passengerTypes).forEach(type => {
+                        if(currentStationData.passengerTypes[type] > 0) {
+                            console.log(type, currentStationData.passengerTypes[type]);
+                        }
+                    });
+                });                
             }
         })
     }
